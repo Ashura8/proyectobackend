aa7162823db1ddd87cd52e093d119563c1545717
Mario <gonzalez_mario@uadec.edu.mx>
2025-05-16 18:02:06 -0600

    Instalación de dependencias y configuración de package.json

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🌐 Servidor escuchando en el puerto ${PORT}`);
+});
+
 
 app.use(cors());
 app.use(express.json());
